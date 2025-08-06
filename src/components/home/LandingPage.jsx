1f98ff5802d634cb9f34f3073bd6a008b6ce9d00
cherlton <cherltonmhangwana@mail.com>
2025-08-06 10:51:26 +0200

    second push

--- src/components/home/LandingPage.jsx
+++ src/components/home/LandingPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'; 
 import { useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
-import videoBack from '../../backgroundVideo/video_back.mp4';
+import videoBack from '../../../public/video_back.mp4';
 
 const LandingPage = () => {
   const navigate = useNavigate();
